5d59d64752695f13941c65df8dda4d727446e1b7
Vonr <dtaclanlegion@gmail.com>
2022-12-06 13:52:12 +0800

    Clean up Day 6 using Unique

--- src/solutions/year2022/day06.rs
+++ src/solutions/year2022/day06.rs
@@ -3,20 +3,21 @@ use std::{
     fmt::Display,
 };
 
-pub fn part1(input: &str) -> impl Display {
+use crate::helper::util::Unique;
+
+pub fn solve(input: &str, size: usize) -> impl Display {
     input
         .as_bytes()
-        .array_windows::<4>()
-        .position(|[a, b, c, d]| a != b && a != c && a != d && b != c && b != d && c != d)
+        .windows(size)
+        .position(Unique::unique)
         .unwrap()
-        + 4
+        + size
+}
+
+pub fn part1(input: &str) -> impl Display {
+    solve(input, 4)
 }
 
 pub fn part2(input: &str) -> impl Display {
-    input
-        .as_bytes()
-        .array_windows::<14>()
-        .position(|arr| arr.iter().collect::<HashSet<_>>().len() == arr.len())
-        .unwrap()
-        + 14
+    solve(input, 14)
 }
