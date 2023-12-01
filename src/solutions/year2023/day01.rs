1192663f196d35a7fcc737eabd15c153a04824c7
Vonr <dtaclanlegion@gmail.com>
2023-12-01 21:52:55 +0800

    Clean up compensation strategy for 2023 d1p1 and implement for d1p2

--- src/solutions/year2023/day01.rs
+++ src/solutions/year2023/day01.rs
@@ -8,7 +8,8 @@ use bstr::ByteSlice;
 
 pub fn part1(input: &str) -> impl Display {
     let input = input.as_bytes();
-    let mut sums: [u32; 2] = [0, 0];
+    let mut lsum: u32 = 0;
+    let mut rsum: u32 = 0;
 
     let mut num_lines: u32 = 0;
     for line in input.lines() {
@@ -16,24 +17,27 @@ pub fn part1(input: &str) -> impl Display {
         let mut iter = line.iter().copied();
         let first = unsafe { iter.find(|&b| b <= b'9').unwrap_unchecked() };
         let last = iter.rfind(|&b| b <= b'9').unwrap_or(first);
-        sums[0] += first as u32;
-        sums[1] += last as u32;
+        lsum += first as u32;
+        rsum += last as u32;
     }
 
     const COMPENSATION: u32 = 11 * b'0' as u32;
 
-    sums[0] * 10 + sums[1] - num_lines * COMPENSATION
+    lsum * 10 + rsum - num_lines * COMPENSATION
 }
 
 pub fn part2(input: &str) -> impl Display {
     let mut input = input.as_bytes();
-    let mut sum: u32 = 0;
+    let mut lsum: u32 = 0;
+    let mut rsum: u32 = 0;
 
     let mut options: [&[u8]; 9] = [
         b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
     ];
 
+    let mut num_lines: u32 = 0;
     for line in input.lines() {
+        num_lines += 1;
         let mut first = MaybeUninit::uninit();
 
         let mut first_idx = 0;
@@ -59,7 +63,8 @@ pub fn part2(input: &str) -> impl Display {
             let b = line[idx];
             if b <= b'9' {
                 let value = b - b'0';
-                sum += unsafe { first.assume_init() * 10 + value } as u32;
+                lsum += unsafe { first.assume_init() } as u32;
+                rsum += value as u32;
                 break;
             }
 
@@ -68,7 +73,8 @@ pub fn part2(input: &str) -> impl Display {
             for option in options.iter() {
                 if line.len() - idx >= 3 && line.get(idx..idx + option.len()) == Some(option) {
                     let value = oidx + 1;
-                    sum += unsafe { first.assume_init() * 10 + value } as u32;
+                    lsum += unsafe { first.assume_init() } as u32;
+                    rsum += value as u32;
                     break 'outer;
                 }
                 oidx += 1
@@ -76,5 +82,7 @@ pub fn part2(input: &str) -> impl Display {
         }
     }
 
-    sum
+    const COMPENSATION: u32 = 11 * b'0' as u32;
+
+    lsum * 10 + rsum - num_lines * COMPENSATION
 }
